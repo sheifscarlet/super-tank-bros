fd29593143b9cf2cd07e0672079a4e3cf27cff2a
Artem Bobrov <art.bobro0v@gmail.com>
2024-11-24 22:21:19 -0500

    scout fix

--- Assets/Scripts/Enemies/KamikazeController.cs
+++ Assets/Scripts/Enemies/KamikazeController.cs
@@ -4,10 +4,7 @@ using UnityEngine.AI;
 
 public class KamikazeController : MonoBehaviour
 {
-    [Header("Movement")]
-    [SerializeField] private float minSpeed = 2;
-    [SerializeField] private float maxSpeed = 4;
-    private float moveSpeed;
+    
 
     [Header("Players")]
     [SerializeField] private GameObject player1; 
@@ -27,8 +24,7 @@ public class KamikazeController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        moveSpeed = Random.Range(minSpeed, maxSpeed);
-        _agent.speed = moveSpeed;
+        
     }
 
     // Update is called once per frame
